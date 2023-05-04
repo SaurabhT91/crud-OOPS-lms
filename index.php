217cfe845137d32c90a964a63676fa349cf21245
SAURABH91TIDGAM <saurabhtidgam1991@gmail.com>
2023-05-04 12:06:11 +0530

    index page changes

--- index.php
+++ index.php
@@ -1,78 +1,99 @@
- <?php
-    // Start session
-    session_start();
+<?php
+// Start session
+session_start();
 
-    // Get data from session
-    $session_Data = !empty($_SESSION['session_Data'])?$_SESSION['session_Data']:'';
+// Get data from session
+$session_Data = !empty($_SESSION['session_Data'])?$_SESSION['session_Data']:'';
 
-    // Get status from session
-    if(!empty($session_Data['status']['msg'])){
-        $statusMsg = $session_Data['status']['msg'];
-        $status = $session_Data['status']['type'];
-        unset($_SESSION['session_Data']['status']);
-    }
+// Get status from session
+if(!empty($session_Data['status']['msg'])){
+    $statusMsg = $session_Data['status']['msg'];
+    $status = $session_Data['status']['type'];
+    unset($_SESSION['session_Data']['status']);
+}
 
-    // Include and initialize DB class
-    require_once 'database_class.php';
-    $db = new DB();
+// Include and initialize DB class
 
-    // Fetch the users data
-    $users = $db->getRows('lead_data', array('order_by'=>'id DESC'));
+require_once 'database_class.php';
+$db = new DB();
 
-    // Retrieve status message from session
-    if(!empty($_SESSION['statusMsg'])){
-        echo '<p>'.$_SESSION['statusMsg'].'</p>';
-        unset($_SESSION['statusMsg']);
-    }
-    ?>
+// Fetch the users data
+$users = $db->get_all_leads('lead_data');
+$users2 = $db->get_all_users('USERS');
+//    var_dump($users2);
+//    die();
+// Retrieve status message from session
+if(!empty($_SESSION['statusMsg'])){
+    echo '<p>'.$_SESSION['statusMsg'].'</p>';
+    unset($_SESSION['statusMsg']);
+}
 
-    <div class="row">
-        <div class="col-md-12 head">
-            <h5>Lead Data</h5>
-            <!-- Add link -->
-            <div class="float-right">
-                <a href="form.php" class="btn btn-success"><i class="plus"></i>Add New Lead</a>
-            </div>
+
+
+?>
+
+<div class="row">
+    <div class="col-md-12 head">
+        <h5>Lead Data</h5>
+        <!-- Add link -->
+        <div class="float-right">
+            <a href="form.php" class="btn btn-success"><i class="plus"></i>Add New Lead</a>
         </div>
+        <form action="action_performed.php" method="post">
+            <label>Search by </label>
+            <select id="ID_TYPE" name="ID_TYPE">
+
+                <option value="USER_ID">USER ID</option>
+                <option value="LEAD_ID">LEAD ID</option>
+
+            </select>
+        </form>
 
         <!-- Status message -->
         <?php if(!empty($statusMsg)){ ?>
             <div class="alert alert-<?php echo $status; ?>"><?php echo $statusMsg; ?></div>
         <?php } ?>
 
-        <!-- List of lead -->
+        <!-- List the users -->
         <table class="table table-striped table-bordered">
             <thead class="thead-dark">
             <tr>
                 <th width="5%">id</th>
                 <th width="10%">Name</th>
                 <th width="10%">Contact number</th>
-                <th width="20%">Address</th>
-                <th width="15%">City</th>
-                <th width="14%">State</th>
+                <th width="30%">Address</th>
+                <th width="12%">City</th>
+                <th width="15%">State</th>
                 <th width="15%">Employment</th>
-                <th width="10%">Loan</th>
+                <th width="5%">Loan</th>
+                <th width="5">USER_ID</th>
             </tr>
             </thead>
             <tbody>
-            <?php if(!empty($users)){ $i=0; foreach($users as $row){ $i++; ?>
-            <tr>
-                <td><?php echo $i; ?></td>
-                <td><?php echo $row['Lead_name']; ?></td>
-                <td><?php echo $row['Contact_number']; ?></td>
-                <td><?php echo $row['Address']; ?></td>
-                <td><?php echo $row['City']; ?></td>
-                <td><?php echo $row['State_name']; ?></td>
-                <td><?php echo $row['Employment_type']; ?></td>
-                <td><?php echo $row['Loan_status']; ?></td>
-                <td>
-                    <a href="form.php?id=<?php echo $row['id']; ?>" class="btn btn-warning">Update</a>
-                    <a href="action_performed.php?action_type=delete&id=<?php echo $row['id']; ?>" class="btn btn-danger" onclick="return confirm('Are you sure to delete data?');">delete</a>
-                </td>
-            </tr>
+            <?php if(!empty($users)) { $i=0;foreach($users as $row) { $i++; ?>
+                <tr>
+                    <td><?php echo $i; ?></td>
+                    <td><?php echo $row['Lead_name']; ?></td>
+                    <td><?php echo $row['Contact_number']; ?></td>
+                    <td><?php echo $row['Address']; ?></td>
+                    <td><?php echo $row['City']; ?></td>
+                    <td><?php echo $row['State_name']; ?></td>
+                    <td><?php echo $row['Employment_type']; ?></td>
+                    <td><?php echo $row['Loan_status']; ?></td>
+                    <td><?php echo $row['USER_ID']; ?></td>
+
+                    <td>
+                        <a href="form.php?id=<?php echo $row['id']; ?>" class="btn btn-warning">Update</a>
+                        <a href="action_performed.php?action_type=delete&id=<?php echo $row['id']; ?>" class="btn btn-danger" onclick="return confirm('Are you sure to delete data?');">delete</a>
+                    </td>
+                </tr>
             <?php } }else{ ?>
-            <tr><td colspan="5">No user(s) found...</td></tr>
+                <tr><td colspan="5">No user(s) found...</td></tr>
             <?php }  ?>
+
+
+
+
             </tbody>
         </table>
     </div>
