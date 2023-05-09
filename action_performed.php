7660171e57b75a9d27dfdf767f70c63e05e2bf4a
SAURABH91TIDGAM <saurabhtidgam1991@gmail.com>
2023-05-09 13:46:24 +0530

    Update action_performed.php

--- action_performed.php
+++ action_performed.php
@@ -14,83 +14,108 @@ $redirectURL = 'index.php';
 
 
 //echo $_REQUEST['action_type'];
-//die();+
+//die();
+function valid_phone($phone){
+    if(preg_match('/^[0-9]{10}+$/', $phone))
+    {
+        return "1";
+    }
+    else{
+        return "0";
+    }
+}
+
+
+
 
 // If Add request is submitted
 if(!empty($_REQUEST['action_type']) && $_REQUEST['action_type'] == 'add') {
     $redirectURL = 'user_page.php';
     // Get user's input
     $postData = $_POST;
-    $name = !empty($_POST['Lead_name'])?trim($_POST['Lead_name']):'';;
-    $phone = !empty($_POST['Contact_number'])?trim($_POST['Contact_number']):'';;
-    $address = !empty($_POST['Address'])?trim($_POST['Address']):'';;
-    $city = !empty($_POST['City'])?trim($_POST['City']):'';
-    $State_name = !empty($_POST['State_name'])?trim($_POST['State_name']):'';
-    $employment = !empty($_POST['Employment_type'])?trim($_POST['Employment_type']):'';
-    $Loan = !empty($_POST['Loan_status'])?trim($_POST['Loan_status']):'';
-    $USER_ID =$_SESSION['USER_ID'];
+    $name = !empty($_POST['Lead_name'])?trim($_POST['Lead_name']):'';
+    $phone = !empty($_POST['Contact_number'])?trim($_POST['Contact_number']):'';
+    $validity = valid_phone($phone);
+//    echo "validity : ". $validity;
+//    die();
+    if ($validity)
+    {
+        $address = !empty($_POST['Address'])?trim($_POST['Address']):'';
+        $city = !empty($_POST['City'])?trim($_POST['City']):'';
+        $State_name = !empty($_POST['State_name'])?trim($_POST['State_name']):'';
+        $employment = !empty($_POST['Employment_type'])?trim($_POST['Employment_type']):'';
+        $Loan = !empty($_POST['Loan_status'])?trim($_POST['Loan_status']):'';
+        $USER_ID =$_SESSION['USER_ID'];
 
 
-    // Validate form fields
-    if (empty($name)) {
-        $valErr .= 'Please enter your name.<br/>';
-    }
-    if (empty($phone)) {
-        $valErr .= 'Please enter your phone no.<br/>';
-    }
-    if (empty($address)) {
-        $valErr .= 'Please enter your phone no.<br/>';
-    }
-    if (empty($city)) {
-        $valErr .= 'Please choose city<br/>';
-    }
-    if (empty($state)) {
-        $valErr .= 'Please choose city<br/>';
-    }
-    if (empty($employment)) {
-        $valErr .= 'Please choose employment type<br/>';
-    }
-    if (empty($loan)) {
-        $valErr .= 'Please enter leads loan status.<br/>';
-    }
-
-
-    // Check whether user inputs are empty
-    if (!empty($valErr)) {
-        // Insert data into the database
-        $userData = array(
-            'Lead_name' => $name,
-            'Contact_number' => $phone,
-            'Address' => $address,
-            'City' => $city,
-            'State_name' => $State_name,
-            'Employment_type' => $employment,
-            'Loan_status' => $Loan,
-            'USER_ID'=>$USER_ID,
-        );
-//            var_dump($leadData);
-        $insert = $db->insert_lead($userData,$USER_ID);
-
-        if ($insert) {
-            $status = 'success';
-            $statusMsg = 'User data has been added successfully!';
-            $postData = '';
-
-            $redirectURL = 'index.php';
-        } else {
-            $statusMsg = 'Something went wrong, please try again after some time.';
+        // Validate form fields
+        if (empty($name)) {
+            $valErr .= 'Please enter your name.<br/>';
         }
-    } else {
-        $statusMsg = '<p>Please fill all the mandatory fields:</p>' . trim($valErr, '<br/>');
+        if (empty($phone)) {
+            $valErr .= 'Please enter your phone no.<br/>';
+        }
+        if (empty($address)) {
+            $valErr .= 'Please enter your phone no.<br/>';
+        }
+        if (empty($city)) {
+            $valErr .= 'Please choose city<br/>';
+        }
+        if (empty($state)) {
+            $valErr .= 'Please choose city<br/>';
+        }
+        if (empty($employment)) {
+            $valErr .= 'Please choose employment type<br/>';
+        }
+        if (empty($loan)) {
+            $valErr .= 'Please enter leads loan status.<br/>';
+        }
+
+
+        // Check whether user inputs are empty
+        if (!empty($valErr)) {
+            // Insert data into the database
+            $userData = array(
+                'Lead_name' => $name,
+                'Contact_number' => $phone,
+                'Address' => $address,
+                'City' => $city,
+                'State_name' => $State_name,
+                'Employment_type' => $employment,
+                'Loan_status' => $Loan,
+                'USER_ID'=>$USER_ID,
+            );
+//            var_dump($leadData);
+            $insert = $db->insert_lead($userData,$USER_ID);
+
+            if ($insert) {
+                $status = 'success';
+                $statusMsg = 'User data has been added successfully!';
+                $postData = '';
+
+                $redirectURL = 'index.php';
+            } else {
+                $statusMsg = 'Something went wrong, please try again after some time.';
+            }
+        } else {
+            $statusMsg = '<p>Please fill all the mandatory fields:</p>' . trim($valErr, '<br/>');
+        }
+
+        // Store status into the SESSION
+        $sessData['postData'] = $postData;
+        $sessData['status']['type'] = $status;
+        $sessData['status']['msg'] = $statusMsg;
+        $_SESSION['sessData'] = $sessData;
+
+        header("location: user_page.php"   );
+
+    }
+    else
+    {
+        echo "you provided invalid phone number. ";
+        echo "<a href=form.php >GO BACK</a>";
     }
 
-    // Store status into the SESSION
-    $sessData['postData'] = $postData;
-    $sessData['status']['type'] = $status;
-    $sessData['status']['msg'] = $statusMsg;
-    $_SESSION['sessData'] = $sessData;
-
-    header("location: user_page.php"   );
 }
 elseif(!empty($_REQUEST['action_type']) && $_REQUEST['action_type'] == 'delete' && !empty($_GET['id'])){ // If Delete request is submitted
     // Delete data from the database
@@ -114,82 +139,95 @@ elseif(!empty($_REQUEST['action_type']) && $_REQUEST['action_type'] == 'delete' 
 elseif(!empty($_REQUEST['action_type']) && $_REQUEST['action_type'] == 'update' && !empty($_POST['id']))
 { // If Edit request is submitted
     $redirectURL = 'form.php?id=' . $_POST['id'];
-
+    $id=$_POST['id'];
     // Get user's input
     $postData = $_POST;
     $name = !empty($_POST['Lead_name']) ? trim($_POST['Lead_name']) : '';
     $phone = !empty($_POST['Contact_number']) ? trim($_POST['Contact_number']) : '';
-    $address = !empty($_POST['Address']) ? trim($_POST['Address']) : '';
-    $city = !empty($_POST['City'])?trim($_POST['City']):'';
-    $State_name = !empty($_POST['State_name'])?trim($_POST['State_name']):'';
-    $employment = !empty($_POST['Employment_type'])?trim($_POST['Employment_type']):'';
-    $Loan = !empty($_POST['Loan_status'])?trim($_POST['Loan_status']):'';
-    $User_Id = $_SESSION['USER_ID'];
 
-    // Validate form fields
-    if (empty($name)) {
-        $valErr .= 'Please enter your name.<br/>';
+    $validity = valid_phone($phone);
+//    echo "validity : ". $validity;
+//    die();
+    if ($validity === "0")
+    {
+        echo "you provided invalid phone number. ";
+        echo "<a href=form.php?id=$id>GO BACK</a>";
     }
-    if (empty($phone)) {
-        $valErr .= 'Please enter your phone no.<br/>';
-    }
-    if (empty($address)) {
-        $valErr .= 'Please enter your phone no.<br/>';
-    }
-    if (empty($city)) {
-        $valErr .= 'Please choose city<br/>';
-    }
-    if (empty($state)) {
-        $valErr .= 'Please choose city<br/>';
-    }
-    if (empty($employment)) {
-        $valErr .= 'Please choose employment type<br/>';
-    }
-    if (empty($loan)) {
-        $valErr .= 'Please enter your loan status.<br/>';
-    }
-    if (empty($User_ID)) {
-        $valErr .= 'Please enter your loan status.<br/>';
-    }
+    else
+    {
+        $address = !empty($_POST['Address']) ? trim($_POST['Address']) : '';
+        $city = !empty($_POST['City'])?trim($_POST['City']):'';
+        $State_name = !empty($_POST['State_name'])?trim($_POST['State_name']):'';
+        $employment = !empty($_POST['Employment_type'])?trim($_POST['Employment_type']):'';
+        $Loan = !empty($_POST['Loan_status'])?trim($_POST['Loan_status']):'';
+        $User_Id = $_SESSION['USER_ID'];
 
-    // Check whether user inputs are empty
-    // Check whether user inputs are empty
-    if (!empty($valErr)) {
-        // Insert data into the database
-        $userData = array(
-            'Lead_name' => $name,
-            'Contact_number' => $phone,
-            'Address' => $address,
-            'City' => $city,
-            'State_name' => $State_name,
-            'Employment_type' => $employment,
-            'Loan_status' => $Loan,
-            'User_Id'=>$User_Id,
-
-        );
-        $conditions = array('id' => $_POST['id']);
-        $update = $db->update_lead($userData, $conditions,$User_Id);
-
-        if ($update) {
-            $status = 'success';
-            $statusMsg = 'User data has been updated successfully!';
-            $postData = '';
-
-            $redirectURL = 'index.php';
-        } else {
-            $statusMsg = 'Something went wrong, please try again after some time.';
+        // Validate form fields
+        if (empty($name)) {
+            $valErr .= 'Please enter your name.<br/>';
         }
-    } else {
-        $statusMsg = '<p>Please fill all the mandatory fields:</p>' . trim($valErr, '<br/>');
+        if (empty($phone)) {
+            $valErr .= 'Please enter your phone no.<br/>';
+        }
+        if (empty($address)) {
+            $valErr .= 'Please enter your phone no.<br/>';
+        }
+        if (empty($city)) {
+            $valErr .= 'Please choose city<br/>';
+        }
+        if (empty($state)) {
+            $valErr .= 'Please choose city<br/>';
+        }
+        if (empty($employment)) {
+            $valErr .= 'Please choose employment type<br/>';
+        }
+        if (empty($loan)) {
+            $valErr .= 'Please enter your loan status.<br/>';
+        }
+        if (empty($User_ID)) {
+            $valErr .= 'Please enter your loan status.<br/>';
+        }
+
+        // Check whether user inputs are empty
+        // Check whether user inputs are empty
+        if (!empty($valErr)) {
+            // Insert data into the database
+            $userData = array(
+                'Lead_name' => $name,
+                'Contact_number' => $phone,
+                'Address' => $address,
+                'City' => $city,
+                'State_name' => $State_name,
+                'Employment_type' => $employment,
+                'Loan_status' => $Loan,
+                'User_Id'=>$User_Id,
+
+            );
+            $conditions = array('id' => $_POST['id']);
+            $update = $db->update_lead($userData, $conditions,$User_Id);
+
+            if ($update) {
+                $status = 'success';
+                $statusMsg = 'User data has been updated successfully!';
+                $postData = '';
+
+                $redirectURL = 'index.php';
+            } else {
+                $statusMsg = 'Something went wrong, please try again after some time.';
+            }
+        } else {
+            $statusMsg = '<p>Please fill all the mandatory fields:</p>' . trim($valErr, '<br/>');
+        }
+
+        // Store status into the SESSION
+        $sessData['postData'] = $postData;
+        $sessData['status']['type'] = $status;
+        $sessData['status']['msg'] = $statusMsg;
+        $_SESSION['sessData'] = $sessData;
+
+        header("location: user_page.php");
     }
 
-    // Store status into the SESSION
-    $sessData['postData'] = $postData;
-    $sessData['status']['type'] = $status;
-    $sessData['status']['msg'] = $statusMsg;
-    $_SESSION['sessData'] = $sessData;
-
-    header("location: user_page.php");
 
 }
 elseif(!empty($_REQUEST['action_type']) && $_REQUEST['action_type'] == 'search' && !empty($_POST['search']))
