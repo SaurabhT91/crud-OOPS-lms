29ba6ae8a29bccf4d41a780031b89e7ae6e05399
SAURABH91TIDGAM <saurabhtidgam1991@gmail.com>
2023-05-19 15:13:08 +0530

    Create user_page.js

---
+++ user_page.js
@@ -0,0 +1,22 @@
+function deleteLeadById(id){
+  var deleteID=id;
+  fetch('http://localhost:7008/action_performed.php?'+deleteID, {'method': 'DELETE'})
+.then(res => res.text())
+.then(data => {
+    alert(data)
+})
+}
+
+function updateLeadById(id){
+
+  location.href = "/form.php?id="+id;
+
+}
+
+
+const signout = document.querySelector('#signOut')
+signout.addEventListener('click', () => location.href = "/logout.php")
+
+const addLead = document.querySelector('#new_Lead')
+addLead.addEventListener('click', () => location.href = "/form.php")
+
